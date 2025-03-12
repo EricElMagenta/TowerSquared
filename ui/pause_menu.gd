e78eb2ce9118f797b296836fe269771c9ee66730
EricElMagenta <fabian.lolli@gmail.com>
2025-03-12 14:03:43 -0300

    Agregado menú de pausa con las opciones de continuar y salir del juego. Agregado botón de pausa a la interfaz. Para esto se usan paneles personalizados

---
+++ ui/pause_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+# SETTER
+var is_paused:bool = false:
+	set(value):
+		is_paused = value
+		get_tree().paused = is_paused
+		visible = is_paused
+	
+########################## FUNCIÓN PRINCIPAL ##############################
+func _unhandled_input(event:InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		is_paused = !is_paused
+
+########################## FUNCIONES AUXILIARES ##############################
+func _on_resume_button_pressed():
+	is_paused = false
+
+
+func _on_exit_level_button_pressed():
+	get_tree().quit()
+
+func _on_pause_button_pressed():
+	is_paused = true
